15fe7d3e954a922f3c00c2307556aca87ff06551
Jean Carlos Ospino González <devjc23@gmail.com>
2018-06-17 23:17:36 -0500

    Agregada la capacidad de editar información de perfil propio

--- src/partials/profile/Info.vue
+++ src/partials/profile/Info.vue
@@ -2,47 +2,41 @@
   <div class="row">
     <div class="col-lg-6 col-md-12">
       <div class="dashboard-list-box margin-top-0">
-        <h4 class="gray">Profile Details</h4>
+        <h4 class="gray">Detalles del perfil</h4>
         <div class="dashboard-list-box-static">
           
           <!-- Avatar -->
           <div class="edit-profile-photo">
-            <img src="http://via.placeholder.com/500x500" alt="">
+            <img :src="user.photoUrl || 'http://via.placeholder.com/500x500'" :alt="'Foto de perfil de' + user.name">
             <div class="change-photo-btn">
               <div class="photoUpload">
                   <span><i class="fa fa-upload"></i> Upload Photo</span>
                   <input type="file" class="upload" />
               </div>
             </div>
+            <button class="button margin-top-15">Guardar cambios</button>
           </div>
 
           <!-- Details -->
-          <div class="my-profile">
+          <form @submit.prevent="saveUserProfile">
+            <div class="my-profile">
+              <label>Nombre</label>
+              <input type="text" v-model="userProfile.name">
 
-            <label>Your Name</label>
-            <input value="Tom Perrin" type="text">
+              <label>Número telefónico</label>
+              <input type="number" v-model="userProfile.phoneNumber">
 
-            <label>Phone</label>
-            <input value="(123) 123-456" type="text">
+              <label>E-mail</label>
+              <input type="email" :value="firebaseUser.email" readonly>
 
-            <label>Email</label>
-            <input value="tom@example.com" type="text">
+              <label>Breve reseña</label>
+              <textarea name="bio" id="bio" cols="30" rows="10" v-model="userProfile.bio"></textarea>
 
-            <label>Notes</label>
-            <textarea name="notes" id="notes" cols="30" rows="10">Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper</textarea>
-
-            <label><i class="fa fa-twitter"></i> Twitter</label>
-            <input placeholder="https://www.twitter.com/" type="text">
-
-            <label><i class="fa fa-facebook-square"></i> Facebook</label>
-            <input placeholder="https://www.facebook.com/" type="text">
-
-            <label><i class="fa fa-google-plus"></i> Google+</label>
-            <input placeholder="https://www.google.com/" type="text">
-          </div>
-
-          <button class="button margin-top-15">Save Changes</button>
-
+              <label>Edad</label>
+              <input type="number" v-model.number="userProfile.age">
+            </div>
+            <button type="submit" class="button margin-top-15">Guardar cambios</button>
+          </form>
         </div>
       </div>
     </div>
@@ -72,3 +66,46 @@
     </div>
   </div>
 </template>
+
+<script>
+  import { DB } from "@/firebase"
+  import { Store } from "@/store"
+  export default {
+    created() {
+      this.setUserProfile()
+    },
+    data() {
+      return {
+        userProfile: {}
+      }
+    },
+    computed: {
+      firebaseUser() {
+        return Store.state.firebaseUser
+      },
+      user() {
+        return Store.state.user
+      }
+    },
+    methods: {
+      setUserProfile() {
+        this.userProfile = Object.assign({}, {
+          name: this.user.name || null,
+          phoneNumber: this.user.phoneNumber || null,
+          email: this.user.email || null,
+          bio: this.user.bio || null,
+          age: this.user.age || null
+        })
+      },
+      saveUserProfile() {
+        const userProfileRef = "/users/users/" + this.firebaseUser.uid + "/profileInfo"
+        DB.ref(userProfileRef).update(this.userProfile)
+        .then(() => {
+          DB.ref(userProfileRef).once("value", snapshot => {
+            this.userProfile = snapshot.val()
+          }).then(() => console.info("Información actualizada."))
+        })
+      }
+    }
+  }
+</script>
