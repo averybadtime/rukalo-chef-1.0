b00fd0ae373c15bcc800209463841188a6874bf3
Jean Carlos Ospino González <devjc23@gmail.com>
2018-06-27 04:31:03 -0500

    Terminada la lógica para la vista de los chefs favoritos del usuario logueado.

--- src/partials/profile/Favorites.vue
+++ src/partials/profile/Favorites.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="row">
-    <h1>Mis favoritos</h1>
+    <div class="col-md-12">
+      <h1>Mis favoritos</h1>
+      <p v-if="isLoadingFavorites">Obteniendo favoritos...</p>
+      <template v-else>
+        <ul v-if="favorites.length > 0">
+          <li v-for="f in favorites" :key="f.key">{{ f }}</li>
+        </ul>
+      </template>
+    </div>
   </div>
 </template>
+
+<script>
+  import { DB } from "@/firebase"
+  export default {
+    created() {
+      this.getFavorites()
+    },
+    data() {
+      return {
+        isLoadingFavorites: true,
+        favorites: [],
+        favoritesRef: DB.ref("/users/users/" + this.$store.state.firebaseUser.uid + "/favorites"),
+        onChildAdded: null,
+        onChildRemoved: null
+      }
+    },
+    methods: {
+      getFavorites() {
+        this.onChildAdded = this.favoritesRef.on("child_added", snapshot => {
+          const chefId = snapshot.key
+          DB.ref("/users/chefs/" + chefId + "/profile").once("value", chefProfileSnapshot => {
+            this.favorites.push({
+              key: chefId,
+              data: chefProfileSnapshot.val()
+            })
+          })
+          this.isLoadingFavorites = false
+        })
+        this.onChildRemoved = this.favoritesRef.on("child_removed", snapshot => {
+          this.favorites = this.favorites.filter(x => snapshot.key !== x.key)
+        })
+      }
+    },
+    beforeDestroy() {
+      this.favoritesRef.off("child_added", this.onChildAdded)
+      this.favoritesRef.off("child_removed", this.onChildRemoved)
+    }
+  }
+</script>
